b46d3bb0d8382fc989590081fe8c13ec82c59a07
heitanmiao <heitanmiao02@aliyun.com>
2019-01-02 08:51:26 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Login from '@/components/login.vue'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/'
+      name: 'home',
+      path: '/',
+      redirect: {
+        name: 'login'
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
